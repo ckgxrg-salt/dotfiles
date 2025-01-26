41381b7e063d3079776b63716be3e769e61ceaba
ckgxrg <ckgxrg@ckgxrg.io>
2025-01-26 23:39:34 +0800

    rofi & fcitx: move to `apps`

---
+++ radilopa/home/apps/rofi.nix
@@ -0,0 +1,26 @@
+{
+  config,
+  pkgs,
+  ckgs,
+  ...
+}:
+{
+  # Rofi
+  programs.rofi = {
+    enable = true;
+    package = pkgs.rofi-wayland;
+    cycle = true;
+    font = "Maple Mono 16";
+    location = "top-right";
+    plugins = with pkgs; [
+      rofi-emoji-wayland
+    ];
+    terminal = "${pkgs.alacritty}/bin/alacritty";
+    theme = "${ckgs.rofi-themes}/config/rofi/launchers/type-3/style-1.rasi";
+    extraConfig = {
+      show-icons = true;
+      icon-theme = config.ckgxrg.themes.icon.name;
+      run-command = "uwsm app -- {cmd}";
+    };
+  };
+}
--- shared/apps/fcitx.nix
+++ shared/apps/fcitx.nix
@@ -5,11 +5,11 @@
   i18n.inputMethod = {
     enabled = "fcitx5";
     fcitx5.addons = with pkgs; [
-      fcitx5-chinese-addons
-      fcitx5-nord
+      fcitx5-fluent
       fcitx5-rime
     ];
   };
+
   # Add fcitx5 to GTK settings
   gtk = {
     gtk2.extraConfig = "gtk-im-module=\"fcitx\"";
