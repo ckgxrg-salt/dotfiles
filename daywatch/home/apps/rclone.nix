c417e665338a071853d1a285b4a2eb637563d0be
ckgxrg <ckgxrg@ckgxrg.io>
2025-04-19 17:22:01 +0800

    rclone: Add auto sync

--- daywatch/home/apps/rclone.nix
+++ daywatch/home/apps/rclone.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, pkgs, ... }:
 # Syncs files from Nextcloud
 {
   programs.rclone = {
@@ -13,6 +13,35 @@
         };
         secrets = {
           pass = "/run/system-keys/paralace-passwd";
+        };
+      };
+    };
+  };
+
+  # Auto-sync some directories
+  systemd.user = let
+  base = {
+    Unit.Requires = [ "network-online.target" ];
+    Service.Type = "oneshot";
+    Install.WantedBy = [ "multi-user.target" ];
+  };
+  base-timer = {
+    Unit.Requires = [ "network-online.target" ];
+    Install.WantedBy = [ "timers.target" ];
+  };
+  in {
+    services = {
+      "rclone-Music" = base // {
+        Unit.Description = "Sync Music with rclone";
+        Service.ExecStart = "${pkgs.rclone}/bin/rclone bisync ${config.xdg.userDirs.music} paralace:Music";
+      };
+    };
+    timers = {
+      "rclone-Music" = base-timer // {
+        Unit.Description = "Sync Music with rclone";
+        Timer = {
+          OnBootSec = "1h";
+          OnUnitActiveSec = "1h";
         };
       };
     };
