448df411367b14fa70c3d2a1a79c0f1141db465c
ckgxrg <ckgxrg@ckgxrg.io>
2025-05-11 11:59:17 +0800

    Rhyslow: Load default openrgb profile at startup

--- shared/gaming/openrgb.nix
+++ shared/gaming/openrgb.nix
@@ -7,5 +7,13 @@
     motherboard = "intel";
   };
 
-  environment.systemPackages = [pkgs.openrgb-with-all-plugins];
+  environment.systemPackages = [ pkgs.openrgb-with-all-plugins ];
+
+  systemd.services."openrgb-default-profile" = {
+    description = "Load default OpenRGB profile";
+    requisite = [ "openrgb.service" ];
+    after = [ "openrgb.service" ];
+    script = "${pkgs.openrgb}/bin/openrgb --profile default";
+    wantedBy = [ "openrgb.service" ];
+  };
 }
