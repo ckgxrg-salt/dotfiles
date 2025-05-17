050e437d51bd832035f56699fd7c070cc04e6f04
ckgxrg <ckgxrg@ckgxrg.io>
2025-05-17 19:11:12 +0800

    Atuin: Use daemon to workaround ZFS

--- home/terminal/accessories.nix
+++ home/terminal/accessories.nix
@@ -24,6 +24,7 @@ in
       enableBashIntegration = config.programs.bash.enable;
       enableZshIntegration = config.programs.zsh.enable;
       enableNushellIntegration = config.programs.nushell.enable;
+      daemon.enable = true;
       flags = [ "--disable-up-arrow" ];
     };
 
