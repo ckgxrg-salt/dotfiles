f6e7a84f752ae491060ad5422ab1ee87dbe8e450
ckgxrg <ckgxrg@ckgxrg.io>
2025-08-18 04:03:42 +0800

    nixvim: fix lua for cmp

--- home/development/nixvim/plugins/completions.nix
+++ home/development/nixvim/plugins/completions.nix
@@ -3,6 +3,10 @@
     cmp = {
       enable = true;
       autoEnableSources = true;
+      luaConfig.pre = ''
+        local luasnip = require('luasnip')
+        local cmp = require('cmp')
+      '';
       settings = {
         snippet = {
           expand = "function(args) require('luasnip').lsp_expand(args.body) end";
@@ -36,39 +40,41 @@
           { name = "path"; }
         ];
         mapping.__raw = ''
-          ['<CR>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              if luasnip.expandable() then
-                luasnip.expand()
+          {
+            ['<CR>'] = cmp.mapping(function(fallback)
+              if cmp.visible() then
+                if luasnip.expandable() then
+                  luasnip.expand()
+                else
+                  cmp.confirm({
+                    select = true,
+                  })
+                end
               else
-                cmp.confirm({
-                  select = true,
-                })
+                fallback()
               end
-            else
-              fallback()
-            end
-          end),
+            end),
 
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.locally_jumpable(1) then
-              luasnip.jump(1)
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
+            ["<Tab>"] = cmp.mapping(function(fallback)
+              if cmp.visible() then
+                cmp.select_next_item()
+              elseif luasnip.locally_jumpable(1) then
+                luasnip.jump(1)
+              else
+                fallback()
+              end
+            end, { "i", "s" }),
 
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.locally_jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
+            ["<S-Tab>"] = cmp.mapping(function(fallback)
+              if cmp.visible() then
+                cmp.select_prev_item()
+              elseif luasnip.locally_jumpable(-1) then
+                luasnip.jump(-1)
+              else
+                fallback()
+              end
+            end, { "i", "s" }),
+          }
         '';
       };
     };
