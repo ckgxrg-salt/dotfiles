b2b4c25e0b71f6ba5b894ff88ad096288d9cbd27
ckgxrg <ckgxrg@ckgxrg.io>
2025-10-03 13:30:33 +0100

    neovim: disable haskell repl

--- home/development/nixvim/plugins/langs/haskell.nix
+++ home/development/nixvim/plugins/langs/haskell.nix
@@ -16,6 +16,5 @@
   extraFiles."after/ftplugin/haskell.lua".text = ''
     vim.keymap.set('n', '<leader>re', require('haskell-tools').lsp.buf_eval_all, { desc = 'Haskell: Evaluate all snippets', buffer = true })
     vim.keymap.set('n', '<leader>cs', require('haskell-tools').hoogle.hoogle_signature, { desc = 'Haskell: Show signature using Hoogle', buffer = true })
-    vim.keymap.set('n', '<leader>rr', require('haskell-tools').repl.toggle, { desc = 'Haskell: Toggle REPL', buffer = true })
   '';
 }
