221c9fab52f79a4f6e4d75e9f964fd595ecf9b55
ckgxrg <ckgxrg@ckgxrg.io>
2025-10-11 22:31:58 +0100

    neovim: correct neotest keymap

--- home/development/neovim/plugins/neotest.lua
+++ home/development/neovim/plugins/neotest.lua
@@ -5,6 +5,6 @@ require("neotest").setup({
 })
 
 vim.keymap.set("n", "<leader>rt", function() require("neotest").run.run() end, { desc = "Run nearest test" })
-vim.keymap.set("n", "<leader>rf", function() require("neotest").run.run() end, { desc = "Run nearest test" })
+vim.keymap.set("n", "<leader>rf", function() require("neotest").run.run(vim.fn.expand("%")) end, { desc = "Run tests in this file" })
 vim.keymap.set("n", "<leader>rs", require("neotest").summary.toggle, { desc = "Show tests summary" })
-vim.keymap.set("n", "<leader>ro", function() require("neotest").output.open({ enter = true }) end, { desc = "Show tests summary" })
+vim.keymap.set("n", "<leader>ro", function() require("neotest").output.open({ enter = true }) end, { desc = "Show test output" })
