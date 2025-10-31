0965b3537d67fef36afec8745ab4c949c5d4f086
ckgxrg <ckgxrg@ckgxrg.io>
2025-10-31 01:21:02 +0000

    neovim: set vimtex to use xelatex

--- home/development/neovim/plugins/langs.lua
+++ home/development/neovim/plugins/langs.lua
@@ -55,3 +55,15 @@ vim.lsp.enable("texlab")
 vim.g.vimtex_view_general_viewer = "okular"
 vim.g.vimtex_view_general_options = "--unique file:@pdf#src:@line@tex"
 vim.g.vimtex_compiler_method = "latexmk"
+vim.g.vimtex_compiler_latexmk_engines = {
+	["_"] = "-xelatex",
+	["pdfdvi"] = "-pdfdvi",
+	["pdfps"] = "-pdfps",
+	["pdflatex"] = "-pdf",
+	["luatex"] = "-lualatex",
+	["lualatex"] = "-lualatex",
+	["xelatex"] = "-xelatex",
+	["context (pdftex)"] = "-pdf -pdflatex=texexec",
+	["context (luatex)"] = "-pdf -pdflatex=context",
+	["context (xetex)"] = "-pdf -pdflatex='texexec --xtx'",
+}
