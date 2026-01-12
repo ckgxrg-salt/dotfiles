fbb22a03933e97d6c3d120ee5d71e4be32d27581
ckgxrg <ckgxrg@ckgxrg.io>
2026-01-12 21:46:34 +0000

    atuin: lift workarounds for zfs

--- home/terminal/accessories.nix
+++ home/terminal/accessories.nix
@@ -28,7 +28,6 @@ in
       enableBashIntegration = config.programs.bash.enable;
       enableZshIntegration = config.programs.zsh.enable;
       enableNushellIntegration = config.programs.nushell.enable;
-      daemon.enable = true;
       flags = [ "--disable-up-arrow" ];
     };
 
