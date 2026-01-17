9a4f2910efe86bd69a3e2dad595d278fb6b9a70f
ckgxrg <ckgxrg@ckgxrg.io>
2026-01-17 23:16:53 +0000

    locale: update settings

--- os/misc/locale.nix
+++ os/misc/locale.nix
@@ -14,34 +14,29 @@ in
   };
 
   config = mkIf cfg.default {
-    # Timezone, Locale
     services.timesyncd.enable = false;
     services.chrony.enable = true;
 
     time.timeZone = mkIf (!cfg.autoTimezone) cfg.timezone;
 
-    services.tzupdate.enable = cfg.autoTimezone;
+    services.tzupdate.enable = mkIf cfg.autoTimezone true;
     systemd.timers = mkIf cfg.autoTimezone {
-      tzupdate.timerConfig.OnCalendar = mkForce null;
+      tzupdate.timerConfig = {
+        OnCalendar = mkForce null;
+        Persistent = mkForce null;
+      };
     };
 
     i18n = {
       defaultLocale = "en_GB.UTF-8";
-      supportedLocales = [
+      extraLocales = [
         "zh_CN.UTF-8/UTF-8"
-        "en_GB.UTF-8/UTF-8"
         "ja_JP.UTF-8/UTF-8"
       ];
       extraLocaleSettings = {
-        LC_ADDRESS = "en_GB.UTF-8";
-        LC_IDENTIFICATION = "zh_CN.UTF-8";
-        LC_MEASUREMENT = "zh_CN.UTF-8";
-        LC_MONETARY = "zh_CN.UTF-8";
-        LC_NAME = "zh_CN.UTF-8";
-        LC_NUMERIC = "zh_CN.UTF-8";
-        LC_PAPER = "zh_CN.UTF-8";
-        LC_TELEPHONE = "zh_CN.UTF-8";
-        LC_TIME = "zh_CN.UTF-8";
+        # yyyy/mm/dd
+        LC_TIME = "en_DK.UTF-8";
+        LC_COLLATE = "C.UTF-8";
       };
     };
   };
