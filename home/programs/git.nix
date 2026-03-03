155bc1f5ac7699619b1a2a4733c0ec0eee83e5ef
ckgxrg <ckgxrg@ckgxrg.io>
2026-03-03 12:26:58 +0000

    git: set explicit signing key

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -12,13 +12,16 @@ in
     programs.git = {
       enable = true;
       lfs.enable = true;
+      signing = {
+        signByDefault = true;
+        key = "AC41 E882 B518 0369 8DCC 67F6 7050 0A63 15A4 4858";
+      };
       settings = {
         user = {
           name = "ckgxrg";
           email = "ckgxrg@ckgxrg.io";
         };
         pull.rebase = true;
-        commit.gpgsign = true;
       };
     };
   };
