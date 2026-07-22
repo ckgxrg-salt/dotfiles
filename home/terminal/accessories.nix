3e170666e83db7260aea533141c6a6a883577e0f
ckgxrg <ckgxrg@ckgxrg.io>
2026-07-22 08:40:31 +0800

    theme: matugen for vivid

--- home/terminal/accessories.nix
+++ home/terminal/accessories.nix
@@ -13,7 +13,10 @@ in
   };
 
   config = mkIf cfg.enable {
-    # TODO: matugen
+    theme.matugen.templates.vivid = {
+      input_path = ../theme/templates/vivid.yml;
+      output_path = "${config.xdg.configHome}/vivid/themes/matugen.yml";
+    };
 
     programs.pay-respects = {
       enable = true;
@@ -34,6 +37,7 @@ in
 
     programs.vivid = {
       enable = true;
+      activeTheme = "matugen";
     };
   };
 }
