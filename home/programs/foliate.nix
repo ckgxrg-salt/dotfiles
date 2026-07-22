1a458a130cdf7eeef430d4ca4a691e9224a086d6
ckgxrg <ckgxrg@ckgxrg.io>
2026-07-22 10:32:29 +0800

    theme: matugen for foliate

--- home/programs/foliate.nix
+++ home/programs/foliate.nix
@@ -1,21 +1,31 @@
-{ config, lib, ... }:
-with lib;
+{
+  osConfig,
+  config,
+  lib,
+  ...
+}:
 let
   cfg = config.program.foliate;
 in
 {
   options.program.foliate = {
-    enable = mkEnableOption "Enable default Foliate settings";
+    enable = lib.mkEnableOption "Enable default Foliate settings";
   };
 
-  config = mkIf cfg.enable {
-    # TODO: matugen for foliate
+  config = lib.mkIf cfg.enable {
+    theme.matugen.templates.foliate = {
+      input_path = ../theme/templates/foliate.json;
+      output_path = "${config.xdg.configHome}/com.github.johnfactotum.Foliate/themes/matugen.json";
+    };
 
     programs.foliate = {
       enable = true;
       settings = {
         "viewer/font" = {
-          default-size = 24;
+          default-size = osConfig.theme.fonts.sizes.applications;
+        };
+        "viewer/view" = {
+          theme = "matugen.json";
         };
       };
     };
