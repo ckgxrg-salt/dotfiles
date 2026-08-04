ea6613541d3987c2f4cdcbf546283996c301924b
ckgxrg <ckgxrg@ckgxrg.io>
2026-08-04 11:46:34 +0800

    scripts: fix icon and cooldown

--- hosts/rhyslow/overrides/scripts.nix
+++ hosts/rhyslow/overrides/scripts.nix
@@ -9,13 +9,13 @@ let
     get_icon() {
       current=$(get_volume)
       if [[ "$current" -eq "0" ]]; then
-        echo audio-volume-low-zero
+        echo audio-volume-muted-symbolic
       elif [[ ("$current" -ge "0") && ("$current" -le "30") ]]; then
-        echo audio-volume-low
+        echo audio-volume-low-symbolic
       elif [[ ("$current" -ge "30") && ("$current" -le "60") ]]; then
-        echo audio-volume-medium
+        echo audio-volume-medium-symbolic
       elif [[ ("$current" -ge "60") && ("$current" -le "100") ]]; then
-        echo audio-volume-high
+        echo audio-volume-high-symbolic
       fi
     }
     notify_user() {
@@ -29,7 +29,7 @@ let
     }
     toggle_mute() {
       if [ "$(wpctl get-volume @DEFAULT_SINK@ | awk '{print $3}')" == "" ]; then
-        wpctl set-mute @DEFAULT_SINK@ 1 && notify-send -t 5000 -h string:x-canonical-private-synchronous:sys-notify -u low -i audio-volume-muted "Muted"
+        wpctl set-mute @DEFAULT_SINK@ 1 && notify-send -t 5000 -h string:x-canonical-private-synchronous:sys-notify -u low -i audio-volume-muted-symbolic "Muted"
       elif [ "$(wpctl get-volume @DEFAULT_SINK@ | awk '{print $3}')" == "[MUTED]" ]; then
         wpctl set-mute @DEFAULT_SINK@ 0 && notify-send -t 5000 -h string:x-canonical-private-synchronous:sys-notify -u low -i "$(get_icon)" "Unmuted"
       fi
@@ -56,16 +56,14 @@ let
     }
     get_icon() {
       current="$(get_backlight)"
-      if [[ ("$current" -ge "0") && ("$current" -le "20") ]]; then
-    	icon="weather-tornado"
-      elif [[ ("$current" -ge "20") && ("$current" -le "40") ]]; then
-    	icon="weather-clear-night"
-      elif [[ ("$current" -ge "40") && ("$current" -le "60") ]]; then
-    	icon="brightness-low"
-      elif [[ ("$current" -ge "60") && ("$current" -le "80") ]]; then
-    	icon="brightness-high"
-      elif [[ ("$current" -ge "80") && ("$current" -le "100") ]]; then
-        icon="weather-clear"
+      if [[ ("$current" -ge "0") && ("$current" -le "25") ]]; then
+        icon="display-brightness-off-symbolic"
+      elif [[ ("$current" -ge "25") && ("$current" -le "50") ]]; then
+        icon="display-brightness-low-symbolic"
+      elif [[ ("$current" -ge "50") && ("$current" -le "75") ]]; then
+        icon="display-brightness-medium-symbolic"
+      elif [[ ("$current" -ge "75") && ("$current" -le "100") ]]; then
+        icon="display-brightness-high-symbolic"
       fi
     }
     notify_user() {
